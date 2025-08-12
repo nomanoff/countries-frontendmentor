fef9c992737b9a54cbf11ca7fc0a10b4aa867553
Ishonch <farruhsaibzanov@gmail.com>
2025-08-13 00:03:47 +0500

    Yangi o‘zgarishlar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -91,7 +91,14 @@ export default function Home({ darkMode }) {
           onChange={(e) => setSearch(e.target.value)}
           dark={darkMode}
         />
-
+<Select value={region} onChange={(e) => setRegion(e.target.value)} dark={darkMode}>
+          <option value="All">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </Select>
       </Controls>
 
       <Grid>
